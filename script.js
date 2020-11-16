3c584e58c2dfd35e68158bb74272c3f2cc1326c1
nickj4495 <nickj4495@gmail.com>
2020-11-16 02:04:58 -0800

    added animated icon group to bottom of each image

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const imageContainer = document.getElementById("image-container");
+const gallery = document.getElementById("gallery");
 const loader = document.getElementById("loader");
 
 let ready = false;
@@ -34,6 +34,10 @@ function displayPhotos() {
   totalImages = photosArray.length;
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
+    // Create <div> image-container
+    const container = document.createElement("div");
+    container.className = "image-container";
+
     // Create <a> to link to Unsplash
     const item = document.createElement("a");
     setAttributes(item, {
@@ -49,9 +53,30 @@ function displayPhotos() {
     });
     // Event Listener, check when each is finished loading
     img.addEventListener("load", imageLoaded);
-    // Put <img> inside <a>, then put both inside imageContainer Element
+    // Put <img> inside <a>, then put both inside gallery Element
+    gallery.appendChild(container);
+    container.appendChild(item);
     item.appendChild(img);
-    imageContainer.appendChild(item);
+    container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="image-icons">
+      <svg class="svg-icon" viewBox="0 0 20 20">
+        <path
+          d="M9.719,17.073l-6.562-6.51c-0.27-0.268-0.504-0.567-0.696-0.888C1.385,7.89,1.67,5.613,3.155,4.14c0.864-0.856,2.012-1.329,3.233-1.329c1.924,0,3.115,1.12,3.612,1.752c0.499-0.634,1.689-1.752,3.612-1.752c1.221,0,2.369,0.472,3.233,1.329c1.484,1.473,1.771,3.75,0.693,5.537c-0.19,0.32-0.425,0.618-0.695,0.887l-6.562,6.51C10.125,17.229,9.875,17.229,9.719,17.073 M6.388,3.61C5.379,3.61,4.431,4,3.717,4.707C2.495,5.92,2.259,7.794,3.145,9.265c0.158,0.265,0.351,0.51,0.574,0.731L10,16.228l6.281-6.232c0.224-0.221,0.416-0.466,0.573-0.729c0.887-1.472,0.651-3.346-0.571-4.56C15.57,4,14.621,3.61,13.612,3.61c-1.43,0-2.639,0.786-3.268,1.863c-0.154,0.264-0.536,0.264-0.69,0C9.029,4.397,7.82,3.61,6.388,3.61"
+        ></path>
+      </svg>
+      <svg class="svg-icon" viewBox="0 0 20 20">
+        <path
+          d="M17.211,3.39H2.788c-0.22,0-0.4,0.18-0.4,0.4v9.614c0,0.221,0.181,0.402,0.4,0.402h3.206v2.402c0,0.363,0.429,0.533,0.683,0.285l2.72-2.688h7.814c0.221,0,0.401-0.182,0.401-0.402V3.79C17.612,3.569,17.432,3.39,17.211,3.39M16.811,13.004H9.232c-0.106,0-0.206,0.043-0.282,0.117L6.795,15.25v-1.846c0-0.219-0.18-0.4-0.401-0.4H3.189V4.19h13.622V13.004z"
+        ></path>
+      </svg>
+      <svg class="svg-icon" viewBox="0 0 20 20">
+        <path
+          d="M3.24,7.51c-0.146,0.142-0.146,0.381,0,0.523l5.199,5.193c0.234,0.238,0.633,0.064,0.633-0.262v-2.634c0.105-0.007,0.212-0.011,0.321-0.011c2.373,0,4.302,1.91,4.302,4.258c0,0.957-0.33,1.809-1.008,2.602c-0.259,0.307,0.084,0.762,0.451,0.572c2.336-1.195,3.73-3.408,3.73-5.924c0-3.741-3.103-6.783-6.916-6.783c-0.307,0-0.615,0.028-0.881,0.063V2.575c0-0.327-0.398-0.5-0.633-0.261L3.24,7.51 M4.027,7.771l4.301-4.3v2.073c0,0.232,0.21,0.409,0.441,0.366c0.298-0.056,0.746-0.123,1.184-0.123c3.402,0,6.172,2.709,6.172,6.041c0,1.695-0.718,3.24-1.979,4.352c0.193-0.51,0.293-1.045,0.293-1.602c0-2.76-2.266-5-5.046-5c-0.256,0-0.528,0.018-0.747,0.05C8.465,9.653,8.328,9.81,8.328,9.995v2.074L4.027,7.771z"
+        ></path>
+      </svg>
+    </div>`
+    );
   });
 }
 
